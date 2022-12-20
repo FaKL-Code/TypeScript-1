a975dd0523d6e2551b2249591d3d9f1f4ccdbc89
Julio Facal <starlyn.dev@gmail.com>
2022-12-19 22:18:49 -0300

    reaproveitando codigo + generics

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,7 +1,16 @@
-export class View {
+export class View<T> {
   protected elemento: HTMLElement;
 
   constructor(seletor: string) {
     this.elemento = document.querySelector(seletor);
+  }
+
+  update(model: T) {
+    const template = this.template(model);
+    this.elemento.innerHTML = template;
+  }
+
+  template(model: T): string {
+    throw Error("Implementar o template na classe filha");
   }
 }
